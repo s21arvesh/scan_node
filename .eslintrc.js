0e01e265f728cfc8f346a4ce92d0df9813c862f3
s21arvesh <131661674+s21arvesh@users.noreply.github.com>
2026-02-08 12:27:06 +0530

    Add ESLint configuration

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: 'eslint:recommended',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  rules: {
+    'no-unused-vars': 'error',
+    'no-console': 'warn',
+    'semi': ['error', 'always'],
+    'quotes': ['error', 'single']
+  }
+};
